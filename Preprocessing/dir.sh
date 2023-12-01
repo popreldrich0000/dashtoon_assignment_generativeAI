5dbf4e7c1291d34b2c09ddc11ab3d90e8e8c906b
Shubham Jangid <84173745+popreldrich0000@users.noreply.github.com>
2023-12-01 12:50:05 +0530

    Add files via upload

---
+++ Preprocessing/dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+folder1="cache_dir"
+folder2="data_final"
+folder4="defect_images"
+folder5="diff_images"
+folder6="long_images"
+folder7="raw_data"
+folder8="small_cropped_images"
+folder9="white_images"
+
+
+
+mkdir "$folder1" "$folder2" "$folder4" "$folder5" "$folder6" "$folder7" "$folder8" "$folder9"
+
+

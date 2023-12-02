bc5b4140263acf57a690e171bb316a011e339958
Shubham Jangid <84173745+popreldrich0000@users.noreply.github.com>
2023-12-02 22:57:36 +0530

    Update dir.sh

--- Preprocessing/dir.sh
+++ Preprocessing/dir.sh
@@ -9,9 +9,9 @@ folder6="long_images"
 folder7="raw_data"
 folder8="small_cropped_images"
 folder9="white_images"
+folder10 = "resized_images"
 
 
-
-mkdir "$folder1" "$folder2" "$folder4" "$folder5" "$folder6" "$folder7" "$folder8" "$folder9"
+mkdir "$folder1" "$folder2" "$folder4" "$folder5" "$folder6" "$folder7" "$folder8" "$folder9" "$folder10"
 
 
